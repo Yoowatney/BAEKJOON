96fd939cef7c9328743b2ebc0d1c909592719ef6
yoowatney <mcwatney@naver.com>
2021-04-04 04:45:54 +0900

    1

--- backtracking/2580/2580.c
+++ backtracking/2580/2580.c
@@ -1,137 +1,75 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int		num_arr[10] = {0, };
-int		zero_arr[9] = {0, };
-int		g_sign = 1;
-int		cnt;
-
-int	count_zero(int arr[][9])
+void	printData(int arr[][9])
 {
-	int cnt;
-	int i;
-	int j;
-
-	cnt = 0;
-	for (i = 0; i < 9; i++)
-	{
-		for (j = 0; j < 9; j++)
-		{
-			if (arr[i][j] == 0)
-			{
-				cnt++;
-			}
-		}
-	}
-	return (cnt);
-}
-
-void	promising_row(int arr[][9])
-{
-	int j;
-
 	for (int i = 0; i < 9; i++)
 	{
-		for (j = 0; j < 9; j++)
-		{
-			num_arr[arr[i][j]] = 1;
-		}
-		for (j = 1; j < 10; j++)
-		{
-			if (num_arr[j] == 0)
-				return ;
-		}
-	}
-
-
-//	printf("\n\n-----^ input output v-----\n\n");
-//
-//	for (int i = 0; i < 9; i++)
-//	{
-//		for (j = 0; j < 8; j++)
-//		{
-//			printf("%d ", arr[i][j]);
-//		}
-//		printf("%d", arr[i][j]);
-//		printf("\n");
-//	}
-
-	memset(num_arr, 0, sizeof(num_arr));
-	for (int i = 0; i < 9; i++)
-	{
-		for (j = 0; j < 9; j++)
-		{
-			num_arr[arr[j][i]] = 1;
-		}
-		for (j = 1; j < 10; j++)
-		{
-			if (num_arr[j] == 0)
-				return ;
-		}
-		memset(num_arr, 0, sizeof(num_arr));
-	}
-
-	printf("\n\n-----^ input output v-----\n\n");
-
-	for (int i = 0; i < 9; i++)
-	{
-		for (j = 0; j < 8; j++)
+		for (int j = 0; j < 9; j++)
 		{
 			printf("%d ", arr[i][j]);
 		}
-		printf("%d", arr[i][j]);
 		printf("\n");
 	}
-	g_sign = 0;
+}
+
+int	ispromising(int row, int col, int num, int arr[][9])
+{
+	for (int i = 0; i < 9; i++)
+	{
+		if (arr[row][i] == num && i != col)
+		{
+			return (0);
+		}
+	}
+
+	for (int i = 0; i < 9; i++)
+	{
+		if (arr[i][col] == num && i != row)
+			return (0);
+	}
+
+	int rrow = row / 3;
+	int ccol = col / 3;
+	for (int i = 3 * rrow; i < 3 + 3 * rrow; i++)
+	{
+		for (int j = 3 * ccol; j < 3 + 3 * ccol; j++)
+			if (arr[i][j] == num && i != row && j != col)
+				return (0);
+	}
+	return (1);
 }
 
 void	DFS(int arr[][9], int from, int to)
 {
-	int i;
-	int j;
-	int k;
-	int array[10] = {0, };
-
-//	if (from == to)
-//	{
-		promising_row(arr);
-//		return ;
-//	}
-
-	for (i = 0; i < 9; i++)
+	if (from == to)
 	{
-		for (j = 0; j < 9; j++)
-		{
-			array[arr[i][j]] = 1;
-		}
-		for (j = 0; j < 9; j++)
+		printData(arr);
+		exit(0);
+	}
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
 		{
 			if (arr[i][j] == 0)
 			{
-				for (k = 1; k < 10; k++)
-				{
-					if (array[k] == 0)
+				for (int k = 1; k < 10; k++)
+					if (ispromising(i, j, k, arr))
 					{
 						arr[i][j] = k;
-						array[k] = 1;
-//						printf("%d%d : %d\n",i, j, k);
-						if (g_sign)
-							DFS(arr, from + 1, to);
+						DFS(arr, from + 1, to);
 					}
-					array[k] = 0;
-				}
 				arr[i][j] = 0;
 			}
 		}
-		memset(array, 0, sizeof(array));
 	}
 }
 
 int	main()
 {
 	int arr[9][9];
-//	int j;
-	int total;
+	int cnt;
 
 	for (int i = 0; i < 9; i++)
 	{
@@ -142,8 +80,17 @@ int	main()
 				cnt++;
 		}
 	}
-	total = count_zero(arr);
-	DFS(arr, 0, total);
+
+	DFS(arr, 0, cnt);
+//	for (int i = 0; i < 9; i++)
+//	{
+//		for (int j = 0; j < 9; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	printf("%d",cnt);
 
 
 //	for (int i = 0; i < 9; i++)
--- backtracking/2580/test.c
+++ backtracking/2580/test.c
@@ -5,6 +5,20 @@ int zeroArr[100][2] = {0, };
 int		myindex = 0;
 int		isSolve = 0;
 
+
+void printData()
+{
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	return ;
+}
+
 int	ispromising(int row, int col, int num)
 {
 	for (int i = 0; i < 9; i++)
@@ -29,41 +43,25 @@ int	ispromising(int row, int col, int num)
 	return (1);
 }
 
-void printData()
-{
-	for (int i = 0; i < 9; i++)
-	{
-		for (int j = 0; j < 9; j++)
-		{
-			printf("%d ", arr[i][j]);
-		}
-		printf("\n");
-	}
-	return ;
-}
-
 void	DFS(int i)
 {
+	if (i == myindex)
+	{
+		printData();
+		isSolve = 1;
+		return ;
+	}
 	for (int n = 1; n < 10; n++)
 	{
 		if (ispromising(zeroArr[i][0], zeroArr[i][1], n))
 		{
 			arr[zeroArr[i][0]][zeroArr[i][1]] = n;
-			if (i + 1 == myindex)
-			{
-				printData();
-				isSolve = 1;
-				return ;
-			}
 			if (!isSolve)
 				DFS(i + 1);
 			arr[zeroArr[i][0]][zeroArr[i][1]] = 0;
 		}
 	}
 }
-
-
-
 
 int	main()
 {
